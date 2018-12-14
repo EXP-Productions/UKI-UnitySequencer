be6021f6013c55d3ddd0ca222c197269989f5b81
Brad Hammond <brad@exp.org.au>
2018-12-14 14:24:54 +1100

    setting up linear actuators

---
+++ UKI-UnitySequencer/Assets/Actuator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Actuator : MonoBehaviour
+{
+    public int _ActuatorIndex = 0;
+
+    public float _MaxLinearTravel = 50;       // Maximum that that linear actuator can travel
+    public float _CurrentLinearLength = 0;    // Current length that the linear actuator is at
+
+    public Vector3 _LocalRotationAxis = Vector3.right;
+    public float _RotationBase = 0;         // Roation of the joint when it is zeroed/calibrated
+    public float _RotationExtended = 30;    // Roation of the joint when it is full extended
+
+    // Send message to calibrate actuator to zero
+    public void CalibrateToZero()
+    {
+        // UDP out to calibrate
+    }
+
+    public void OnCalibrationCompleteHandler()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position + (transform.TransformDirection(_LocalRotationAxis) *.1f),
+            transform.position + (transform.TransformDirection(_LocalRotationAxis) * .5f));
+
+        for (int i = 0; i < 12; i++)
+        {
+            float angle = 0;
+            //Gizmos.DrawLine(transform.position);
+        }
+    }
+
+}
---
+++ UKI-UnitySequencer/Assets/Leg.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+// Maps rotations to linear values and back again for leg articulations, so they can be animated or scripted
+public class Leg : MonoBehaviour
+{
+    public Actuator _Hip;
+    public Actuator _Knee;
+    public Actuator _Ankle;   
+   
+    
+    void Start ()
+    {
+		
+	}
+	
+	void CalibrateAllToZero()
+    {
+        _Hip.CalibrateToZero();
+        _Knee.CalibrateToZero();
+        _Ankle.CalibrateToZero();
+    }
+
+    void OnCalibrationCompleteHandler()
+    {
+        _Hip.OnCalibrationCompleteHandler();
+        _Knee.OnCalibrationCompleteHandler();
+        _Ankle.OnCalibrationCompleteHandler();
+    }
+
+    void OnDrawGizmos()
+    {
+        //Gizmos.DrawLine(_Hip.)
+    }
+}
