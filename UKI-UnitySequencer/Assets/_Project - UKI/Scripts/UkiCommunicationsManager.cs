6614e6e8ae36decb8e57a5d6f994ed91c43ea423
Stephen Burns <steve@exp.org.au>
2018-12-17 17:45:16 +1100

    Adding uki comms manager class that will handle actuator update message sends

--- UKI-UnitySequencer/Assets/_Project - UKI/Scripts/UkiCommunicationsManager.cs
+++ UKI-UnitySequencer/Assets/_Project - UKI/Scripts/UkiCommunicationsManager.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UkiCommunicationsManager : MonoBehaviour
+public class UkiCommunicationsManager : ThreadedUDPReceiver
 {
+    private static uint[] _HeartBeatMessage = new uint[] {240, 0, 0};
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SendHeartBeat", 1f, 1f);   
+    }
+    
+    void SendHeartBeat()
+    {
+        SendInts(_HeartBeatMessage);
     }
 
     // Update is called once per frame
