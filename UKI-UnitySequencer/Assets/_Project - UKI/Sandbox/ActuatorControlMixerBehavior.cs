8f24c0b05e1318fc00db556b0ad4b3b68287666c
BradHammond <brad@exp.org.au>
2019-08-28 11:59:38 +1000

    timeline animation with custom clips sorted

---
+++ UKI-UnitySequencer/Assets/_Project - UKI/Sandbox/ActuatorControlMixerBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActuatorControlMixerBehavior : PlayableBehaviour
+{   
+    public float _NormExtention = 1f;
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        Actuator actuator = playerData as Actuator;
+        float finalNorm = 0f;
+
+        if (!actuator)
+        {
+            Debug.Log("here");
+            return;
+        }
+
+        // Get the number of all clips on this track
+        int inputCount = playable.GetInputCount();
+
+        for (int i = 0; i < inputCount; i++)
+        {
+            float inputWeight = playable.GetInputWeight(i);
+            ScriptPlayable<ActuatorControlBehavior> inputPlayable = (ScriptPlayable<ActuatorControlBehavior>)playable.GetInput(i);
+            ActuatorControlBehavior input = inputPlayable.GetBehaviour();
+
+            // Use the above variables to process each frame of this playable.
+            finalNorm += input._NormExtention * inputWeight;
+        }
+
+        //assign the result to the bound object
+        actuator._NormExtension = finalNorm;
+    }
+}
