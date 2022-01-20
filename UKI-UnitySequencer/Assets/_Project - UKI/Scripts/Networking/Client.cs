0617b374ff18ce09d8da0f8495b8254654f040b1
BradHammond <brad@exp.org.au>
2022-01-20 21:44:43 +1100

    Got a simple TCP client server setup sending and recieving byte arrays

---
+++ UKI-UnitySequencer/Assets/_Project - UKI/Scripts/Networking/Client.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    bool _SocketReady;
+    TcpClient _Socket;
+    NetworkStream _Stream;
+    StreamWriter _Writer;
+    StreamReader _Reader;
+
+    private void Start()
+    {
+        ConnectToServer();
+    }
+
+    private void ConnectToServer()
+    {
+        // If already connected return
+        if (_SocketReady)
+            return;
+
+        // Default values
+        string host = "127.0.0.1";
+        int port = 6321;
+
+        try
+        {
+            _Socket = new TcpClient(host, port);
+            _Stream = _Socket.GetStream();
+            _Writer = new StreamWriter(_Stream);
+            _Reader = new StreamReader(_Stream);
+            _SocketReady = true;
+        }
+        catch (Exception e)
+        {
+
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if(_SocketReady)
+        {
+            if(_Stream.DataAvailable)
+            {
+                string data = _Reader.ReadLine();
+                if (data != null)
+                    OnIncomingData(data);
+            }
+        }
+
+        //--  DEBUG
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            WriteBytes(new byte[] { 0, 1, 2, 3, 4, 5, 15, 16, 17, 18, 19, 20 });
+            WriteBytes(new byte[] { 15, 16, 17, 18, 19, 20, 99, 98 });
+            SendBytes();
+        }
+    }
+
+    private void OnIncomingData(string data)
+    {
+        Debug.Log("Server: " + data);
+    }
+
+    void Send(string data)
+    {
+        if (!_SocketReady)
+            return;
+
+        _Writer.WriteLine(data);
+        _Writer.Flush();
+    }
+
+    List<byte> _BytesList = new List<byte>();
+    void WriteBytes(byte[] data)
+    {
+        for (int i = 0; i < data.Length; i++)
+        {
+            _BytesList.Add(data[i]);
+        }
+    }
+
+    void SendBytes()
+    {
+        _BytesList.Insert(0, (byte)_BytesList.Count);
+        _Stream.Write(_BytesList.ToArray(), 0, _BytesList.Count);
+        _Stream.Flush();
+        _BytesList.Clear();
+    }
+
+    void Send(byte[] data)
+    {
+        if (!_SocketReady)
+            return;
+
+        _Stream.Write(data, 0, data.Length);
+        _Stream.Flush();
+    }
+
+    void CloseSocket()
+    {
+        if (!_SocketReady)
+            return;
+
+        _Writer.Close();
+        _Reader.Close();
+        _Socket.Close();
+        _SocketReady = false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseSocket();
+    }
+
+    private void OnDisable()
+    {
+        CloseSocket();
+    }
+}
---
+++ UKI-UnitySequencer/Assets/_Project - UKI/Scripts/Networking/Server.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    public enum ReadType
+    {
+        String,
+        ByteArray
+    }
+
+    List<ServerClient> _Clients = new List<ServerClient>();
+    List<ServerClient> _DisconnectList = new List<ServerClient>();
+
+    public ReadType _ReadType = ReadType.ByteArray;
+    public string _LocalIP;
+    public int _Port = 6321;
+    TcpListener _Server;
+    bool _ServerStarted = false;
+
+    
+
+
+    public string _TestMessage = "Test 1234";
+
+
+    private void Awake()
+    {
+        _Clients = new List<ServerClient>();
+        _DisconnectList = new List<ServerClient>();
+
+        try
+        {
+            _Server = new TcpListener(IPAddress.Any, _Port);
+            _Server.Start();
+            StartListening();
+            _ServerStarted = true;
+
+            _LocalIP = GetLocalIPAddress();
+            Debug.Log("Server started: " + _LocalIP + ":" + _Port);
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void Update()
+    { 
+        if (!_ServerStarted)
+            return;
+
+        foreach(ServerClient c in _Clients)
+        {
+            // Is the client still connected?
+            if(!IsConnected(c._Tcp))
+            {
+                c._Tcp.Close();
+                _DisconnectList.Add(c);
+                continue;
+            }
+            // Check messages from this client
+            else
+            {
+                NetworkStream stream = c._Tcp.GetStream();
+                if(stream.DataAvailable)
+                {
+                    if (_ReadType == ReadType.ByteArray)
+                    {
+                        int count = stream.ReadByte();
+                        byte[] byteData = new byte[count];
+                        stream.Read(byteData, 0, byteData.Length);
+                        if (byteData != null)
+                            OnIncomingData(c, byteData);
+                    }
+                    else
+                    {
+                        StreamReader reader = new StreamReader(stream, true);
+                        string data = reader.ReadLine();
+
+                        if (data != null)
+                            OnIncomingData(c, data);
+                    }
+                }
+            }
+        }
+
+        //--  MANAGE DISCONNECTIONS
+        for (int i = 0; i < _DisconnectList.Count - 1; i++)
+        {
+            Broadcast(_DisconnectList[i]._ClientName + " has disconnected.", _Clients);
+
+            _Clients.Remove(_DisconnectList[i]);
+            _DisconnectList.RemoveAt(i);
+        }
+
+
+        //--  DEBUG
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Broadcast(_TestMessage, _Clients);
+        }
+    }
+
+    private void OnIncomingData(ServerClient c, string data)
+    {
+        Debug.Log(c._ClientName + " has sent: " + data);
+    }
+
+    private void OnIncomingData(ServerClient c, byte[] data)
+    {
+        Debug.Log(c._ClientName + " has sent data of length : " + data.Length);
+        for (int i = 0; i < data.Length; i++)
+        {
+            Debug.Log(i + " - " + data[i]);
+        }       
+    }
+
+    private bool IsConnected(TcpClient tcp)
+    {
+        try 
+        {
+            if (tcp != null && tcp.Client != null && tcp.Client.Connected)
+            {
+                if (tcp.Client.Poll(0, SelectMode.SelectRead))
+                {
+                    return !(tcp.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+                }
+
+                return true;
+            }
+            else
+                return false;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private void StartListening()
+    {
+        _Server.BeginAcceptTcpClient(AcceptTcpClient, _Server);
+    }
+
+    void AcceptTcpClient(IAsyncResult asyncResult)
+    {
+        TcpListener listener = (TcpListener)asyncResult.AsyncState;
+
+        _Clients.Add(new ServerClient(listener.EndAcceptTcpClient(asyncResult)));
+        StartListening();
+
+        //--  Send message to tell everyone a new client has joined
+        Broadcast(_Clients[_Clients.Count - 1]._ClientName + " has connected", _Clients);
+    }
+
+    void Broadcast(string data, List<ServerClient> clients)
+    {
+        foreach (ServerClient c in clients)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(c._Tcp.GetStream());
+                writer.WriteLine(data);
+                writer.Flush();
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Write error: " + e.Message + " to clients " + c._ClientName);
+            }
+        }
+    }
+
+    public static string GetLocalIPAddress()
+    {
+        var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+}
+
+public class ServerClient
+{
+    public TcpClient _Tcp;
+    public string _ClientName;
+
+    public ServerClient(TcpClient clientSocket)
+    {
+        _ClientName = "Guest";
+        _Tcp = clientSocket;
+    }
+}
